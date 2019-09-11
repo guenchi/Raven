f3817b8c9913dd4679533ec74c57940489af9af8
guenchi <louis.deguenchy@gmail.com>
2019-09-11 03:17:44 +0200

    clean

--- raven.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-@set CHEZSCHEMELIBDIRS=.;lib;%UserProfile%\raven
-@set CHEZSCHEMELIBEXTS=.chezscheme.sls;;.chezscheme.so;.ss;;.so;.sls;;.so;.scm;;.so;.sch;;.so;.sc;;.so
-@set "EXE=%UserProfile%\raven\raven\raven.sc"
-scheme --script %EXE% %*
--- raven.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-scheme --libdirs ".:lib:/usr/local/lib" --libexts ".chezscheme.sls::.chezscheme.so:.ss::.so:.sls::.so:.scm::.so:.sch::.so:.sc::.so" --script ./raven.sc
